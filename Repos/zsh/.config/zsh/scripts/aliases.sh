024c0ff5d03a321d0ceb7a6e6c48899f2bcca897
Joseph Hallett <josephhallett@gmail.com>
2016-04-21 18:53:49 +0100

    [zsh] aliases need to handle non-GNU coreutils

--- Repos/zsh/.config/zsh/scripts/aliases.sh
+++ Repos/zsh/.config/zsh/scripts/aliases.sh
@@ -1,5 +1,8 @@
 export LS_COLORS='no=00;37:fi=00:di=00;33:ln=04;36:pi=40;33:so=01;35:bd=40;33;01:'
 
-alias ls='ls --color=auto -p'
-
+if [ "$(uname)" = 'Darwin' ]; then
+    alias ls='ls -G -p'
+else
+    alias ls='ls --color=auto -p'
+fi
 alias -s git='git clone --recursive'
