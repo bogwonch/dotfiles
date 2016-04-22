75488484bfd9763d5e6466970cf5b923858fddff
Joseph Hallett <josephhallett@gmail.com>
2016-04-22 11:42:41 +0100

    [emacs] org pretty entities

--- Repos/emacs/.emacs.d/packages/org-mode.el
+++ Repos/emacs/.emacs.d/packages/org-mode.el
@@ -1,6 +1,7 @@
 (add-hook 'org-mode-hook
 	  '(lambda ()
-	     (org-indent-mode 1)))
+	     (org-indent-mode 1)
+	     (org-toggle-pretty-entities)))
 (req-package org
   :mode ("\\.org\\'" . org-mode))
 (req-package org-ref :require org)
